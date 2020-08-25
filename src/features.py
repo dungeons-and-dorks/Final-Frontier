6eef690ed4f49554eecd09a2e1ca0cf675398663
shayaltshue <shayaltshue@gmail.com>
2020-08-25 10:18:45 -0500

    created a new feature to encode mission_success

---
+++ src/features.py
@@ -0,0 +1,8 @@
+def mission_result(df):
+    # Creating a new column and setting it's default as 1
+    df['mission_result'] = 1
+    
+    # Slicing by the rows that contain a failure, then setting the result to 0
+    df.loc[df.mission_status.str.contains('Failure'), 'mission_result'] = 0
+    
+    return df
